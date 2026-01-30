44cc1d44b6551a080277e350888b9c4ca961a752
ddeliberador <ddeliberador@gmail.com>
2026-01-30 00:18:02 -0300

    style: adicionar cor sóbria slate para diferenciar sidebar

--- src/pages/MvpEngine.tsx
+++ src/pages/MvpEngine.tsx
@@ -115,7 +115,7 @@ const MvpEngine = () => {
 
       <div className="flex pt-16">
         {/* Sidebar */}
-        <aside className="w-80 flex-shrink-0 bg-card border-r border-border overflow-y-auto sticky top-16 h-[calc(100vh-4rem)]">
+        <aside className="w-80 flex-shrink-0 bg-slate-50 dark:bg-slate-900 border-r border-border overflow-y-auto sticky top-16 h-[calc(100vh-4rem)]">
           <div className="p-6 space-y-6">
             {/* Logo and Title */}
             <div className="text-center pb-6 border-b border-border">
